cc0fd7f884902ca315d95e2aa10bda0e8315ad4d
kesia <kesia.salgado@gmail.com>
2025-08-04 20:57:18 -0300

    updateproject

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { transactions } from "./data";
 
 const app = express();
+const port = 3000;
 
 app.get("/", (_req, res) => {
   res.json({ message: "Transactions API" });
@@ -11,8 +12,8 @@ app.get("/transactions", (_req, res) => {
   res.json({ transactions });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
 
 export default app;
